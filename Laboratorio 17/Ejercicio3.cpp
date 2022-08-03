c61d8fb97347abd6930b074f42253dc34a106d43
jeanfcg <104164533+jeanfcg@users.noreply.github.com>
2022-08-03 10:52:51 -0500

    Add files via upload

---
+++ Laboratorio 17/Ejercicio3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+template <class T>
+class Contenedor {
+	T elemento;
+	public :
+	Contenedor (T arg ) {
+		elemento = arg ;
+	}
+	T add() { return ++elemento;}
+};
+
+template <>
+class Contenedor<char> {
+	char elemento;
+	public:
+	Contenedor ( char arg ) {
+		elemento = arg ;
+	}
+	char uppercase() {
+	if ((elemento >= 'a') && (elemento <='z')) {
+		elemento += 'B'-'a';}
+	return elemento ;
+	}
+};
+
+int main(){
+	Contenedor<int> cint (15) ;
+	Contenedor<char> cchar('j');
+	std::cout << cint.add() << std::endl;
+	std::cout << cchar.uppercase() << std::endl;
+	return 0;
+}

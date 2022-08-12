e9367dba952de4f443d56da707428e646961ab40
jeanfcg <104164533+jeanfcg@users.noreply.github.com>
2022-08-11 21:33:02 -0500

    Add files via upload

---
+++ Laboratorio 19/Ejercicio1.cpp
@@ -0,0 +1,79 @@
+#include "iostream"
+#include "vector"
+#include "ctime"
+#include <conio.h>
+
+using namespace std;
+
+class coordinates{
+private:
+	int x = 0;
+	int y = 0;
+	int xy = 0;
+	int xsquare = 0;
+public:
+	coordinates(int _x, int _y) : x(_x), y(_y){
+		xy = _x*_y;
+		xsquare = x*x;
+	}
+	int getX(){return x;}
+	int getY(){return y;}
+	int getXY(){return xy;}
+	int getXCuadrado(){return xsquare;}
+	void print(){
+		cout<<"\t"<<x<<"\t"<<y<<endl;
+	}
+};
+
+class RegresionLinelaSimple{
+private:
+	vector<coordinates> puntos;
+	float suma_xy =0;
+	float suma_xcuadrado=0;
+	float suma_x=0;
+	float suma_y=0;
+	int total=0;
+public:
+	void addElementos(coordinates aux){
+		puntos.push_back(aux);
+	}
+		vector<float> operator()(){
+			total = puntos.size();
+			vector<float> aux;
+			for (int i = 0; i < total; i++){
+				suma_xy += puntos[i].getXY();
+				suma_xcuadrado+=puntos[i].getXCuadrado();
+				suma_x += puntos[i].getX();
+				suma_y += puntos[i].getY();
+			}
+			float m = (total*(suma_xy) - suma_x*suma_y)/ (total*(suma_xcuadrado) - suma_x*suma_x);
+			float b = suma_y/total - m*(suma_x/total);
+			
+			aux.push_back(m);
+			aux.push_back(b);
+			
+			return aux;
+		}
+};
+int main(){
+	vector<coordinates> puntos;
+	RegresionLinelaSimple regresionLineal;
+	
+	int n = 8;
+	srand(time(NULL));
+	cout<<"los datos son generados aleatoriamente"<<endl;
+	cout<<"los datos para construir la ecuacion de regresion lineal son:"<<endl;
+	cout<<"\tX\tY"<<endl;
+	for (int i = 0; i < n; i++){
+		coordinates a( 1+rand()%10 ,1+rand()%10);
+		a.print();
+		regresionLineal.addElementos(a);
+	}
+	
+	vector<float> datos = regresionLineal();
+	cout<<"la ecuacion lineal es: \n\ty = "<<datos[0]<<"x + "<<datos[1]<<endl;
+	
+	getch();
+	return 0;
+	
+}
---
+++ Laboratorio 19/Ejercicio2.cpp
@@ -0,0 +1,62 @@
+#include "iostream"
+#include "vector"
+#include "ctime"
+#include <conio.h>
+
+
+using namespace std;
+
+class Find{
+private:
+	vector<int> datos;        
+public:
+	void anhadirElementos(int aux){
+		datos.push_back(aux);
+	}
+		
+		void imprimirDatos(){
+			for(int i=0; i<datos.size();i++)
+				cout<<datos[i]<<" ";        
+			cout<<endl;
+		}
+			vector<float> operator()(int inicio, int final, int dato){            
+				vector<float> aux;
+				for (int i = inicio; i < final; i++){
+					if(datos[i]==dato)
+						aux.push_back(i);
+				}     
+				return aux;
+			}
+			
+			
+};
+int main(){
+	int n =3;
+	int inicio = 0;
+	int final = 10;
+	Find array;
+	
+	srand(time(NULL));
+	for (int i = 0; i < 10; i++)
+		array.anhadirElementos(1+rand()%+10);    
+	
+	cout<<"los datos del array son: \n\t";
+	array.imprimirDatos();
+	cout<<endl;
+	
+	//Utilizar funtores
+	vector<float> datos = array(inicio,final,n);
+	cout<<"buscar el numero "<<n<<" desde la posicion "<<inicio<<" hasta la posicion "<<final-1<<endl;
+	if(datos.size()>0){
+		cout<<"posiciones donde se ecuentra coincidencia con el numero " <<n<<": \n\t";
+		for(int i=0; i<datos.size();i++)
+			cout<<datos[i]<<" ";    
+		cout<<endl;
+	}else
+	   cout<<"no se encontraron coincidencias con el numero "<<n<<endl;
+	
+	
+	getch();
+	return 0;
+	
+}
